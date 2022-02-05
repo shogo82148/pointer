36eec0dd135a7dbadbad4c9209ebf028eaba1063
Ichinose Shogo <shogo82148@gmail.com>
2022-02-05 14:32:49 +0900

    improve the example

--- example_any_test.go
+++ example_any_test.go
@@ -42,15 +42,17 @@ func ExampleValue() {
 }
 
 func ExampleEqual() {
-	a := 42
-	b := 42
+	// a and b have the same look.
+	a := pointer.Ptr(42)
+	b := pointer.Ptr(42)
 
-	// a and b have same value, but their addresses are differrent.
-	fmt.Printf("&a == &b: %t\n", &a == &b)
+	// But it reports false because a and b have differrent addresses.
+	fmt.Printf("a == b: %t\n", a == b)
 
-	fmt.Printf("pointer.Equal(&a, &b): %t\n", pointer.Equal(&a, &b))
+	// Equal returns true.
+	fmt.Printf("pointer.Equal(a, b): %t\n", pointer.Equal(a, b))
 
 	//Output:
-	// &a == &b: false
-	// pointer.Equal(&a, &b): true
+	// a == b: false
+	// pointer.Equal(a, b): true
 }

ccf32b17158396ffd8f18673f14a2c52990a227c
ICHINOSE Shogo <shogo82148@gmail.com>
2025-12-20 02:24:55 +0900

    Ptr is now deprecated. use new(v) instead

--- pointer_go1.26.go
+++ pointer_go1.26.go
@@ -1,101 +1,177 @@
-//go:build !go1.26
-// +build !go1.26
+//go:build go1.26
+// +build go1.26
 
 package pointer
 
 import "time"
 
 // Int returns a pointer to an int whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Int(v int) *int {
-	return &v
+	return new(v)
 }
 
 // Int8 returns a pointer to an int8 whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Int8(v int8) *int8 {
-	return &v
+	return new(v)
 }
 
 // Int16 returns a pointer to an int16 whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Int16(v int16) *int16 {
-	return &v
+	return new(v)
 }
 
 // Int32 returns a pointer to an int32 whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Int32(v int32) *int32 {
-	return &v
+	return new(v)
 }
 
 // Int64 returns a pointer to an int64 whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Int64(v int64) *int64 {
-	return &v
+	return new(v)
 }
 
 // Uint returns a pointer to an uint whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Uint(v uint) *uint {
-	return &v
+	return new(v)
 }
 
 // Uint8 returns a pointer to an uint8 whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Uint8(v uint8) *uint8 {
-	return &v
+	return new(v)
 }
 
 // Uint16 returns a pointer to an uint16 whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Uint16(v uint16) *uint16 {
-	return &v
+	return new(v)
 }
 
 // Uint32 returns a pointer to an uint32 whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Uint32(v uint32) *uint32 {
-	return &v
+	return new(v)
 }
 
 // Uint64 returns a pointer to an uint64 whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Uint64(v uint64) *uint64 {
-	return &v
+	return new(v)
 }
 
 // String returns a pointer to a string whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func String(v string) *string {
-	return &v
+	return new(v)
 }
 
 // Float32 returns a pointer to a string whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Float32(v float32) *float32 {
-	return &v
+	return new(v)
 }
 
 // Float64 returns a pointer to a string whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Float64(v float64) *float64 {
-	return &v
+	return new(v)
 }
 
 // Bool returns a pointer to a string whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Bool(v bool) *bool {
-	return &v
+	return new(v)
 }
 
 // Byte returns a pointer to a byte whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Byte(v byte) *byte {
-	return &v
+	return new(v)
 }
 
 // Rune returns a pointer to a rune whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Rune(v rune) *rune {
-	return &v
+	return new(v)
 }
 
 // Time returns a pointer to a time.Time whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Time(v time.Time) *time.Time {
-	return &v
+	return new(v)
 }
 
 // Complex64 returns a pointer to a complex64 whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Complex64(v complex64) *complex64 {
-	return &v
+	return new(v)
 }
 
 // Complex128 returns a pointer to a complex128 whose value is v.
+//
+// Deprecated: Use new(v) instead.
+//
+//go:fix inline
 func Complex128(v complex128) *complex128 {
-	return &v
+	return new(v)
 }
